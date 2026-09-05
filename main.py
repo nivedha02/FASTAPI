4838d7293f459dfdf32e68198801f539302f5adf
nivedha02 <76787422+nivedha02@users.noreply.github.com.>
2026-09-05 22:49:16 +0530

    getcode

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+from fastapi import FastAPI
+app=FastAPI()
+
+#@app->decorator (path to go) def function followed by message
+@app.get("/")
+def root():
+    return{"message":"Welcome World!!"} 
+
+@app.get("/post")
+def get_post():
+    return{"data":"This is my post"}
